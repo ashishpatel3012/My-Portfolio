13d195ee150680a8ef18aee3c496ed3600c59c13
ashishpatel3012 <129655846+ashishpatel3012@users.noreply.github.com>
2024-09-07 22:56:30 +0530

    Add home-page

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -2,7 +2,19 @@
 
 export const Homepage = () => {
   return (
-    <div>H\this is home page</div>
+    
+    <>
+    <div className="container flex items-center justify-between px-28 py-28">
+      <div className="text-3xl flex flex-col gap-3">
+        <h1 className="text-5xl text-yellow-700">im Ashish patel</h1>
+        <p>React Devloper </p>
+        <button className=" rounded py-3 bg-yellow-800">Contect Me</button>
+      </div>
+      <div className="bg-red-400">
+        <img src="public/images/dp.jpg" alt="" />
+      </div>
+    </div>
+    </>
   )
 }
 
