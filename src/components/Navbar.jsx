ca6a8fbec4b164d53e88c972c89ba9ead1ee3515
ashishpatel3012 <129655846+ashishpatel3012@users.noreply.github.com>
2024-09-15 15:52:06 +0530

    A-update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 // import img from "./a-logo.png"
-import { GrFormSearch } from "react-icons/gr";
-import { FaReact } from "react-icons/fa";
+// import { GrFormSearch } from "react-icons/gr";
+// import { FaReact } from "react-icons/fa";
 import { FaBars } from "react-icons/fa6";
 
 
@@ -8,31 +8,31 @@ import { FaBars } from "react-icons/fa6";
 const Navbar = () => {
   return (
     <>
-<<<<<<< HEAD
-      <div className="flex items-center justify-between border-b-2 border-yellow-900 border-opacity-40 bg-gradient-to-r    px-8  md:flex md:items-center md:justify-between  ">
-        <div>
-=======
+
+      {/* <div className="flex items-center justify-between border-b-2 border-yellow-900 border-opacity-40 bg-gradient-to-r    px-8  md:flex md:items-center md:justify-between  "> */}
+       
+
       <div className="flex items-center justify-between bg-yellow-900 w-full  px-8  md:flex md:items-center md:justify-between  ">
      
           <div>
->>>>>>> 476a847f21720d5f69f6e86563fdd630d51b6db3
+
           {/* <FaReact className="text-5xl  text-yellow-800" /> */}
           {/* <h1 className="text-2xl  text-yellow-800">AP</h1> */}
 
           <img className="w-14 text-white " src="/images/A-logo2.png " alt="" />
-<<<<<<< HEAD
+
         </div >
           <FaBars className="text-4xl text-black hover:text-yellow-600 md:hidden "/>
         <div className="  w-full  text-right   ">
           
-=======
+
        </div>
         
           <FaBars className="text-4xl hover:text-yellow-600 visible sm:invisible "/>
         
         
         <div className="  w-full  text-right invisible lg:visible md:visible xl:visible">
->>>>>>> 476a847f21720d5f69f6e86563fdd630d51b6db3
+
           <ul className=" ">
             <li className=" ">
               <a
@@ -60,6 +60,7 @@ const Navbar = () => {
           <GrFormSearch className="searchlogo h-10 rounded-r-lg   w-12   bg-gray-700 text-yellow-600 " />
         </div> */}
       </div>
+      
     </>
   );
 };
